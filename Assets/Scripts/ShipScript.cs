cd962cad54c7dfac0a33b6f9990deb5798dca75a
Wolfxela <80600784+Wolfxela@users.noreply.github.com>
2022-11-01 14:51:20 +0300

    Add ship prefab and script with target

--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShipScript : MonoBehaviour
 {
+    [SerializeField] private Transform Target;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class ShipScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Target.position.x);
+        Debug.Log(Target.position.y);
     }
 }
