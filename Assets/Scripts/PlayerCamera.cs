8a76e4986936b669202f00c62ec290fc25d89fe2
Alexledragon <Alexledragon@hotmail.com>
2022-11-01 13:57:47 +0100

    added camera rotation script

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float mouseSensitivity;
+    [SerializeField] float maxVerticalCameraAngle;
+    [SerializeField] float minVerticalCameraAngle;
+
+    float inputMouseX;
+    float inputMouseY;
+    float cameraRotationX;
+    float cameraRotationY;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ManageCursor();
+        RegisterInputs();
+        RotateCamera();
+    }
+
+
+    //---------- CUSTOM FUNCTIONS
+
+    void RegisterInputs()
+    {
+        //----- Register the inputs used by the user
+
+        //register the mouse movement inputs
+        inputMouseX = Input.GetAxis("Mouse X");
+        inputMouseY = Input.GetAxis("Mouse Y");
+    }
+
+    void RotateCamera()
+    {
+        //----- Rotate the camera in relation to the user mouse movements
+
+        //if right click is held down, increment the X and Y camera rotation values, keep X rotation within wanted boundaries and hide the cursor
+        if (Input.GetMouseButton(1))
+        {
+            cameraRotationX -= inputMouseY * mouseSensitivity;
+            cameraRotationX = Mathf.Clamp(cameraRotationX, maxVerticalCameraAngle, minVerticalCameraAngle);
+
+            cameraRotationY += inputMouseX * mouseSensitivity;
+        }
+
+        //apply the obtained rotation values to the camera transform
+        transform.localRotation = Quaternion.Euler(cameraRotationX, cameraRotationY, 0f);
+    }
+
+    void ManageCursor()
+    {
+        //----- Manage the cursor lock and visibility depending on the inputs currently pressed
+
+        //lock and hide the cursor when right click is held
+        if (Input.GetMouseButton(1))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        //confine and show the cursor if no input is pressed
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
     }
 }
